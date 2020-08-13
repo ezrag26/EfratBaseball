90bcd5bc853e443ddda0218800e2b9e90a66c49d
Ezra Gordon <ezrag26@gmail.com>
2020-08-13 05:45:13 +0300

    update forms fields to raise the placeholder text when focused in field or when there is text in the field

--- site/login/index.js
+++ site/login/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Header from "../Header";
+import { onBlur, onClick } from '../helpers/form'
 
 const Login = () => {
   return (
@@ -8,9 +9,18 @@ const Login = () => {
       <Header />
       <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '2em' }}>
         <form>
-          <input type={'email'} name={'email'} placeholder={'Email'}/>
-          <input type={'password'} name={'password'} placeholder={'Password'}/>
+          <div className={'input'}>
+            <input type={'email'} name={'email'} onBlur={onBlur}/>
+            <label className={'label'} htmlFor={'email'} onClick={onClick}>Email</label>
+          </div>
+
+          <div className={'input'}>
+            <input type={'password'} name={'password'} onBlur={onBlur}/>
+            <label className={'label'} htmlFor={'password'} onClick={onClick}>Password</label>
+          </div>
+
           <input className={'form-button'} type={'submit'} value={'Login'}/>
+          <p><a href={'/reset-password'}>Forgot password?</a></p>
           <p>Don't have an account? <a href={'/register'}>Register</a></p>
         </form>
       </div>
--- site/register/index.js
+++ site/register/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Header from "../Header";
+import { onBlur, onClick } from '../helpers/form'
+import { randomBits } from '../helpers/unique'
+
+const DropDown = ({ items }) => {
+  return (
+    <select name={'carrier'} id={'carrier'} style={{ backgroundColor: 'white', fontSize: '1.5rem', width: 'auto', height: 'auto', margin: 'auto 10px 2px auto' }}>
+      { items.map(item => <option key={randomBits()} value={item}>{item}</option>) }
+    </select>
+  )
+}
 
 const Register = () => {
   return (
@@ -8,22 +18,35 @@ const Register = () => {
       <Header />
       <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '2em' }}>
         <form>
-          <input type={'text'} name={'first-name'} placeholder={'First Name'}/>
-          <input type={'text'} name={'last-name'} placeholder={'Last Name'}/>
-          <input type={'email'} name={'email'} placeholder={'Email'}/>
-          <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
-            <select name={'carrier'} id={'carrier'} style={{ backgroundColor: 'white', fontSize: '1.5rem', width: 'auto', height: 'auto', marginBottom: 'auto', marginRight: '10px' }}>
-              <option value={'050'}>050</option>
-              <option value={'051'}>051</option>
-              <option value={'052'}>052</option>
-              <option value={'053'}>053</option>
-              <option value={'054'}>054</option>
-              <option value={'055'}>055</option>
-              <option value={'058'}>058</option>
-            </select>
-            <input type={'number'} name={'phone'} placeholder={'5558184'} min={'0'} max={'9999999'} style={{ marginRight: 'auto', width: '100%' }}/>
+          <div className={'input'}>
+            <input type={'text'} name={'first-name'} onBlur={onBlur}/>
+            <label className={'label'} htmlFor={'first-name'} onClick={onClick}>First Name</label>
           </div>
-          <input type={'password'} name={'password'} placeholder={'Password'}/>
+
+          <div className={'input'}>
+            <input type={'text'} name={'last-name'} onBlur={onBlur}/>
+            <label className={'label'} htmlFor={'last-name'} onClick={onClick}>Last Name</label>
+          </div>
+
+          <div className={'input'}>
+            <input type={'email'} name={'email'} onBlur={onBlur}/>
+            <label className={'label'} htmlFor={'email'} onClick={onClick}>Email</label>
+          </div>
+
+          <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+            <DropDown items={['050', '051', '052', '053', '054', '055', '058']}/>
+
+            <div className={'input'} style={{ width: '100%' }}>
+              <input type={'number'} name={'phone'} onBlur={onBlur} min={'0'} max={'9999999'}/>
+              <label className={'label'} htmlFor={'phone'} onClick={onClick}>Phone Number</label>
+            </div>
+          </div>
+
+          <div className={'input'}>
+            <input type={'password'} name={'password'} onBlur={onBlur}/>
+            <label className={'label'} htmlFor={'password'} onClick={onClick}>Password</label>
+          </div>
+
           <input className={'form-button'} type={'submit'} value={'Register'}/>
           <p>Already have an account? <a href={'/login'}>Login</a></p>
         </form>
