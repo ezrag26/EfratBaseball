f6427757c799a10559299c80f7284ecc6082a9d8
Ezra Gordon <ezrag26@gmail.com>
2020-09-13 01:38:48 +0300

    add some height padding to the dropdown button, as well as proper highlighting of arrow area upon hover, factor out inline css to stylesheet, restyle Input in forms to be sized more correctly, have a filled background, swap the email and phone number rows, and display error messages when needed

--- site/helpers/form.js
+++ site/helpers/form.js
@@ -1,6 +1,5 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { randomBits } from "./unique";
-import { Tab } from './tab'
 
 const FormRow = ({ children }) => {
   return (
@@ -8,42 +7,50 @@ const FormRow = ({ children }) => {
   )
 }
 
-const Input = ({ type, name, placeHolder, value = '', onChange }) => {
+const Input = ({ type, name, placeHolder, value = '', onChange, helpText, error }) => {
   const [raise, setRaise] = useState(false)
   const inputRef = useRef(null)
 
   const onBlur = e => {
     // raise label if input has text inside
-    e.target.value ? setRaise(true) : setRaise(false)
+    setRaise(e.target.value !== '')
   }
 
   const onClick = () => inputRef.current.focus() // focus the input element to raise label
 
   return (
-    <div className={'input'}>
-      <input type={type} name={name} ref={inputRef} onBlur={onBlur} value={value} onChange={e => onChange(e.target.value)}/>
-      <label className={`label ${raise ? 'raise' : ''}`} htmlFor={name} onClick={onClick}>{placeHolder}</label>
-    </div>
+    <>
+      <div className={'input'} onClick={onClick}>
+        <input type={type} name={name} ref={inputRef} value={value} onBlur={onBlur} onChange={e => onChange(e.target.value)}/>
+        <label className={`label ${raise ? 'raise' : ''}`} htmlFor={name}>{placeHolder}</label>
+        <p className={error ? 'error' : 'help'} onClick={e => {}}>{error ? error : helpText}</p>
+      </div>
+    </>
   )
 }
 
 const DropDownMenu = ({ selection, setSelection, items }) => {
   const [hideDropdown, setHideDropdown] = useState(true)
+  const dropdownRef = useRef(null)
+
+  useEffect(() => {
+    if (!hideDropdown) dropdownRef.current.focus()
+  }, [hideDropdown])
 
   return (
-    <div className={'dropdown'} onBlur={() => setHideDropdown(true)} tabIndex={-1}>
-      <div className={'dropdown-btn'} onClick={() => setHideDropdown(false)}>
+    <div className={'dropdown'}>
+      <div className={'dropdown-btn'} style={{ borderRadius: !hideDropdown ? '5px 5px 0 0' : '' }} onClick={() => setHideDropdown(false)}>
         <div>{selection?.name || 'No Items'}</div>
-        <div>&#x25BE;</div>
+        <div style={{ backgroundColor: !hideDropdown ? 'rgba(0, 0, 0, .1)' : '', borderRadius: !hideDropdown ? '0 5px 0 0' : '' }}>&#x25BE;</div>
       </div>
 
-      <ul className={`dropdown-content ${hideDropdown ? 'hidden' : ''}`}>
+      <ul className={`dropdown-content ${hideDropdown ? 'hidden' : ''}`} ref={dropdownRef} onBlur={() => setHideDropdown(true)} tabIndex={-1}>
         {
           items.map(item =>
-            <Tab key={randomBits()} text={`${item.name}`} onClick={() => {
+            <li key={randomBits()} style={{ fontWeight: selection.name === item.name ? '1000' : '' }} onClick={() => {
               setSelection(item)
               setHideDropdown(true)
-            }} />
+            }}>{item.name}</li>
           )
         }
       </ul>
--- site/register/index.js
+++ site/register/index.js
@@ -2,49 +2,54 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import NonAdminHeader from "../NonAdminHeader";
 import {FormRow, Input, DropDownMenu} from '../helpers/form'
-import { XMLHttpRequestAsPromise, fetchPostJson } from '../helpers/request'
+import { fetchPostJson } from '../helpers/request'
 import validator from 'validator';
+import { Stack, Center } from "../helpers/Typography";
+
+const HYPHEN = '-'
+
+const beg3 = new RegExp(/^\d{0,3}$/)
+const full = new RegExp(/^\d{3}.?\d{0,4}$/)
+const onlyNumbers = new RegExp(/^\d{4,7}$/)
+const validNumber = new RegExp(/^\d{3}.?\d{4}$/)
 
 const send = ({ body }) => {
   return fetchPostJson({
     url: '/register',
     body
   })
-    // return XMLHttpRequestAsPromise({
-    //   method: 'POST',
-    //   url: '/register',
-    //   options: {
-    //     body: {
-    //       "first-name": first,
-    //       "last-name": last,
-    //       email,
-    //       carrier,
-    //       phone,
-    //       password
-    //     },
-    //     contentType: "application/json",
-    //     responseType: 'json'
-    //   }
-    // })
 }
+
+const useFormInput = (initialState) => {
+  const [state, setState] = useState(initialState)
+  const [error, setError] = useState()
+
+  const setStateAndError = (newState, newError) => {
+    setState(newState)
+    setError(newError && 'Error: ' + newError)
+  }
+
+  return [state, error, (newState, newError = '') => setStateAndError(newState, newError)]
+}
+
+const carriers = [
+  { name: '050' },
+  { name: '051' },
+  { name: '052' },
+  { name: '053' },
+  { name: '054' },
+  { name: '055' },
+  { name: '058' }
+]
 
 const Register = () => {
   const [first, setFirst] = useState('')
   const [last, setLast] = useState('')
-  const [email, setEmail] = useState('')
-  const carriers = [
-    { name: '050' },
-    { name: '051' },
-    { name: '052' },
-    { name: '053' },
-    { name: '054' },
-    { name: '055' },
-    { name: '058' }
-  ]
+  const [email, emailError, setEmail] = useFormInput('')
   const [carrier, setCarrier] = useState(carriers[0])
-  const [phone, setPhone] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
+  const [phone, phoneError, setPhone] = useFormInput('')
+  const [password, passwordError, setPassword] = useFormInput('')
+  const [confirmPassword, confirmPasswordError, setConfirmPassword] = useFormInput('')
   const [processed, setProcessed] = useState(false)
 
   const validForm = () => {
@@ -52,7 +57,7 @@ const Register = () => {
     last &&
     email && validator.isEmail(email) &&
     carrier.name &&
-    phone &&
+    phone && validNumber.test(phone) &&
     password && password.length >= 5 && confirmPassword === password
   }
 
@@ -67,14 +72,11 @@ const Register = () => {
         "last-name": last,
         email,
         carrier: carrier.name,
-        phone,
+        phone: phone.replace(/[^\d]/, ''),
         password
       }
     })
-      .then(res => {
-        console.log('here')
-        setProcessed(true)
-      })
+      .then(res => setProcessed(true))
       .catch(err => {
       })
   }
@@ -86,10 +88,40 @@ const Register = () => {
   const registerMessage = () => {
     return (
       <>
-        <p>Your information has been successfully received.</p>
-        <p>Please follow the instructions in the email that we have sent to <span style={{ fontWeight: 'bold' }}>{email}</span> in order to complete your registration</p>
+        <Center>
+          <p>Your information has been successfully received.</p>
+        </Center>
+        <Center>
+          <p>Please follow the instructions in the email that we have sent to <span style={{ fontWeight: 'bold' }}>{email}</span> in order to complete your registration</p>
+        </Center>
       </>
     )
+  }
+
+  const maskPhone = v => {
+    const length = v.length
+
+    if ((length <= 3 && !beg3.test(v)) || (length > 3 && length <= 8 && !full.test(v))) {
+      return setPhone(prev => prev, 'Phone Number must be in format ###-####')
+    }
+
+    setPhone(prev => {
+      if (length > 8) return prev // too many numbers, or trying to change hyphen
+
+      if (length > 3) {
+        if (onlyNumbers.test(v)) return v.substr(0, 3).concat(HYPHEN).concat(v.substr(3, length))
+
+        return v.replace(/[^\d]/, HYPHEN)
+      }
+
+      if (length === 3) {
+        if (prev.length > 4) return v.substr(0, 3).concat(HYPHEN)
+        if (prev.length === 4 && prev.substr(0, 3) === v) return v.substr(0, 2) // delete 3rd number => delete the hyphen
+        return v.concat(HYPHEN) // added 3rd number => add hyphen
+      }
+
+      return v
+    }, '')
   }
 
   return processed ? registerMessage() : (
@@ -97,7 +129,11 @@ const Register = () => {
       <NonAdminHeader />
       <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '2em' }}>
         <form onSubmit={onSubmit} onKeyPress={keyPress}>
-          <h1 style={{ textAlign: 'center' }}>Register</h1>
+          <Center>
+            <Stack.Small>
+              <h1>Register</h1>
+            </Stack.Small>
+          </Center>
 
           <FormRow>
             <Input type={'text'} name={'first-name'} placeHolder={'First Name'} value={first} onChange={setFirst}/>
@@ -105,19 +141,22 @@ const Register = () => {
           </FormRow>
 
           <FormRow>
+            <div style={{ margin: 'auto 0 0' }}>
+              <DropDownMenu items={carriers} selection={carrier} setSelection={setCarrier}/>
+            </div>
+            <Input type={'text'} name={'phone'} placeHolder={'Phone Number'} value={phone} onChange={maskPhone} error={phoneError}/>
+          </FormRow>
+
+          <FormRow>
             <Input type={'text'} name={'email'} placeHolder={'Email'} value={email} onChange={setEmail}/>
           </FormRow>
 
           <FormRow>
-            <div style={{ margin: 'auto 5px 0 0' }}>
-              <DropDownMenu items={carriers} selection={carrier} setSelection={setCarrier}/>
-            </div>
-            <Input type={'text'} name={'phone'} placeHolder={'Phone Number'} value={phone} onChange={setPhone}/>
-          </FormRow>
-
-          <FormRow>
             <Input type={'password'} name={'password'} placeHolder={'Password'} value={password} onChange={setPassword}/>
-            <Input type={'password'} name={'confirm-password'} placeHolder={'Confirm Password'} value={confirmPassword} onChange={setConfirmPassword}/>
+            <Input type={'password'} name={'confirm-password'} placeHolder={'Confirm Password'} value={confirmPassword} onChange={v => {
+              if (v.length >= password.length && v !== password) return setConfirmPassword(v, 'Passwords do not match')
+              setConfirmPassword(v)
+            }} error={confirmPasswordError}/>
           </FormRow>
 
           <input className={`form-button ${!validForm() ? 'invalid-form' : ''}`} type={'submit'} value={'Register'} disabled={!validForm()}/>
