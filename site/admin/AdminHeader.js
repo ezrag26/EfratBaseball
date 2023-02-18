7b1fd659b2f7a5e4cba8fc545527e3e540e11a9d
ezra.gordon <ezra.gordon@nexog.com>
2023-02-18 20:51:13 +0200

    Don't show tabs in admin site if not logged in, redirect to admin login if user is no longer logged in within admin site, and redirect non superadmin user to non-admin site from admin site

--- site/admin/AdminHeader.js
+++ site/admin/AdminHeader.js
@@ -4,23 +4,43 @@ import { fetchGetJson } from "../helpers/request";
 import { baseUrl } from '../helpers/api'
 
 const AdminHeader = ({ current }) => {
-  const [user, setUser] = useState(false)
+  const [user, setUser] = useState()
 
   useEffect(() => {
     fetch(`${baseUrl}/me`)
+      .then(res => {
+        if (401 === res.status) {
+          if (!window.location.pathname.includes('login')) {
+            window.location.replace(`/admin/login`)
+          }
+
+          return Promise.reject()
+        }
+
+        return res
+      })
       .then(res => res.json())
+      .then(user => {
+        if (user.role !== 'superadmin') {
+          window.location.replace(`/`)
+          return Promise.reject()
+        }
+
+        return user
+      })
       .then(setUser)
+      .catch(() => setUser(undefined))
   }, [])
 
   return (
     <Header
-      logo={{ href: '/admin', alt: 'admin-homepage' }}
-      nav={[
+      logo={{ href: user ? '/admin' : '/admin/login', alt: 'admin-homepage' }}
+      nav={user ? [
         { text: 'Schedule', href: '/admin/schedule' },
         { text: 'Teams', href: '/admin/teams' },
         { text: 'Leagues', href: '/admin/leagues' },
         { text: 'Gallery', href: '/admin/gallery' }
-      ]}
+      ] : []}
       current={current}
       account={
         !user ? [
